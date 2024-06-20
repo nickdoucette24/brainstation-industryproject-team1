12caaec09314feb640759f379806dcea1067d044
Brandon <brandon.redford@outlook.com>
2024-06-20 08:35:27 -0700

    add gitignore to client root folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from 'react';
+
 
 function App() {
-  return <div></div>;
+  const [ productList, setProductList ] = useState([]);
+
+  return(
+    <>
+    </>
+  )
 }
 
 export default App;
