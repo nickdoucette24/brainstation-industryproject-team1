6d651a19a7a7c2099120634fe5c20278b667737a
Brandon <brandon.redford@outlook.com>
2024-06-22 08:33:42 -0700

    add LandingPage main section

---
+++ client/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,21 @@
+import './LandingPage.scss';
+
+function LandingPage() {
+
+    return(
+        <div className='container'>
+            <div className='main'>
+                <div className='main__text'>
+                    <div className='main__text--page-header'>Track MSRP Compliance Effortlessly</div>
+                    <div className='main__text--subheader'>Empower your decision making with Spectra.<br/>Redefining MSRP Tracking.</div>
+                </div>
+                <div className='main__card'>
+                    <div className='main__card--features'></div>
+                    <div className='main__card--benefits'></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LandingPage;
